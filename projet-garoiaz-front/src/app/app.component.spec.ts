e58ec4246bc13a41c7bdede10ce27eb73edd9397
Radouane Garoiaz <39490675+redo18@users.noreply.github.com>
2022-07-28 14:18:18 +0200

    Update app.component.spec.ts

--- projet-garoiaz-front/src/app/app.component.spec.ts
+++ projet-garoiaz-front/src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'projet-morpion-front'`, () => {
+  it(`should have as title 'projet-garoiaz-front'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('projet-morpion-front');
+    expect(app.title).toEqual('projet-garoiaz-front');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('projet-morpion-front app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('projet-garoiaz-front app is running!');
   });
 });
